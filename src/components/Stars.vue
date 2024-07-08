4c648a2a2d8398d3a1e178add3f05eb2bc18d792
Dan <35927536+dan-mba@users.noreply.github.com>
2024-07-08 09:25:40 -0400

    remove unnecessary span

--- src/components/Stars.vue
+++ src/components/Stars.vue
@@ -1,12 +1,10 @@
 <template>
-  <span>
-    <Rating
-      :model-value="stars"
-      :readonly="true"
-      :cancel="false"
-      :pt="{root: 'inline', onicon: 'ml-1 text-base', officon: 'ml-1 text-base'}"
-    />
-  </span>
+  <Rating
+    :model-value="stars"
+    :readonly="true"
+    :cancel="false"
+    :pt="{root: 'inline', onicon: 'ml-1 text-base', officon: 'ml-1 text-base'}"
+  />
 </template>
 
 <script setup>
