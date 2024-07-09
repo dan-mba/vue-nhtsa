50fa67a4c37192f53c0bd73dd6663c795d7146e3
Dan <35927536+dan-mba@users.noreply.github.com>
2024-07-09 07:44:21 -0400

    change theme color

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {createApp} from 'vue';
 import App from './App';
 import PrimeVue from 'primevue/config';
+import {definePreset} from '@primevue/themes';
 import Aura from '@primevue/themes/aura';
 import Dropdown from 'primevue/dropdown';
 import Card from 'primevue/card';
@@ -9,11 +10,28 @@ import './main.css';
 import 'primeicons/primeicons.css';
 
 const app = createApp(App);
+const MyPreset = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{sky.50}',
+          100: '{sky.100}',
+          200: '{sky.200}',
+          300: '{sky.300}',
+          400: '{sky.400}',
+          500: '{sky.500}',
+          600: '{sky.600}',
+          700: '{sky.700}',
+          800: '{sky.800}',
+          900: '{sky.900}',
+          950: '{sky.950}'
+      }
+  }
+});
 
 app.use(PrimeVue, {
   // Default theme configuration
   theme: {
-    preset: Aura,
+    preset: MyPreset,
     options: {
       prefix: 'p',
       darkModeSelector: 'system',
