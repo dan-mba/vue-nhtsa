6b21e9874df483ca4ac050495ddbebe7a37009b3
Dan <35927536+dan-mba@users.noreply.github.com>
2024-12-12 06:54:35 -0500

    Fix endpoint

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,5 @@
 const endpoint = 'https://api.nhtsa.gov/SafetyRatings';
-const proxy = 'https://corsproxy.io/?';
+const proxy = 'https://corsproxy.io/?url=';
 
 export default function getUrl (URLPart){
   return proxy + encodeURIComponent(`${endpoint}${URLPart}`);
