4bf18c1e5ceb14fd8bc5eb856c8c7ca763f61344
Dan <35927536+dan-mba@users.noreply.github.com>
2026-01-29 06:56:57 -0500

    Change proxy

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,5 @@
 const endpoint = 'https://api.nhtsa.gov/SafetyRatings';
-const proxy = 'https://api.allorigins.win/get?url=';
+const proxy = 'https://cors-anywhere.com/';
 
 export default function getUrl (URLPart){
   return proxy + encodeURIComponent(`${endpoint}${URLPart}`);
