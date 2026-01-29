c72126ad1bfe0694d58104ac74da59cf692535ab
Dan <35927536+dan-mba@users.noreply.github.com>
2026-01-29 06:45:33 -0500

    Switch proxy service

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,5 @@
 const endpoint = 'https://api.nhtsa.gov/SafetyRatings';
-const proxy = 'https://api.cors.lol/?url=';
+const proxy = 'https://cors-proxy.htmldriven.com/?url=';
 
 export default function getUrl (URLPart){
   return proxy + encodeURIComponent(`${endpoint}${URLPart}`);
